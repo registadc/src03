0c77d91ea4358ba4cd8fec9e7f170472b56d721d
giee🦩 <cahyanidwiregista08@gmail.com>
2025-05-14 21:58:13 +0700

    menambahkan 3 file

---
+++ Ulasan.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ulasan;
+
+/**
+ *
+ * @author avita
+ */
+public class Ulasan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Flora kelengkeng = new Flora ();
+        System.out.println("nama pohon :" + kelengkeng.nama);
+        
+        rambutan rambutan01 = new rambutan();
+        System.out.println("jenis rambutan"+ rambutan01.nama);
+    }
+  
+}
